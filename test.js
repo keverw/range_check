000f87eb65777d18939f84b0e6d6a23148861e9c
Kevin <keverw92@gmail.com>
2012-05-24 18:32:24 -0400

    moved test to the tests folder

--- test.js
+++
@@ -1,3 +0,0 @@
-var range_check = require('./range_check.js'); //use just diskspace if installed via NPM
-
-console.log(range_check.vaild_ip('10.0.1.5'));
---
+++ tests/vaildip.js
@@ -0,0 +1,3 @@
+var range_check = require('../range_check.js'); //use just diskspace if installed via NPM
+
+console.log(range_check.vaild_ip('10.0.1.5'));
