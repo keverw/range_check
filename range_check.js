9641d4e9142bf3a7216d5554d3cd195a23d8c65f
Kevin <keverw92@gmail.com>
2012-05-24 20:09:13 -0400

    Ran http://jsbeautifier.org/ and http://www.jshint.com/ on the code

--- range_check.js
+++ range_check.js
@@ -18,48 +18,48 @@
             return 6;
         }
     }
-	
-	function in_range(addr, range)
-	{
-			if (typeof(range) == 'string')
-			{
-				if (range.indexOf('/') !== -1)
-				{
-					var range_data = range.split('/');
-					
-					var ipaddr = require('ipaddr.js');
-					
-					var parse_addr = ipaddr.parse(addr);
-					var range = ipaddr.parse(range_data[0]);
-					
-					return parse_addr.match(range, range_data[1]);
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else if (typeof(range) == 'object')//list
-			{
-				for(var check_range in range)
-				{
-					if (in_range(addr, range[check_range]) == true)
-					{
-						return true;
-					}
-				}
-				return false;
-			}
-			else
-			{
-				return false;
-			}
-	}
+
+    function in_range(addr, range)
+    {
+        if (typeof (range) === 'string')
+        {
+            if (range.indexOf('/') !== -1)
+            {
+                var range_data = range.split('/');
+
+                var ipaddr = require('ipaddr.js');
+
+                var parse_addr = ipaddr.parse(addr);
+                var parse_range = ipaddr.parse(range_data[0]);
+
+                return parse_addr.match(parse_range, range_data[1]);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (typeof (range) === 'object') //list
+        {
+            for (var check_range in range)
+            {
+                if (in_range(addr, range[check_range]) === true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     // Export public API
     var range_check = {};
     range_check.vaild_ip = vaild_ip;
     range_check.ver = ver;
-	range_check.in_range = in_range;
+    range_check.in_range = in_range;
     module.exports = range_check;
 }());
